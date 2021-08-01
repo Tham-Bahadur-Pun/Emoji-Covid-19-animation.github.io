3d3ba782544e785cc8854dee8418d6ef0c099e52
Tham Bahadur Pun <thambahadurpun123@gmail.com>
2021-08-01 13:37:53 +0545

    position of span(eye) is monitored and stored in variable x and y

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.querySelector("body").addEventListener(e=>{
   let eye=document.querySelectorAll(".eye");
+  eye.forEach(eye=>{
+    let x=(eye.getBoundingClientRect().left)+(eye.clientWidth/2);
+    let y=(eye.getBoundingClientRect().top)+(eye.clientHeight/2);
+    
+  })
 })
